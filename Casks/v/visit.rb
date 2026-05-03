b794968d9d6cd8fcb82a4444c1b164f5cfdecfa8
Sam Ford <1584702+samford@users.noreply.github.com>
2026-05-03 17:47:28 -0400

    visit 3.5.0
    `visit` is autobumped but the workflow failed to update to version
    3.5.0 because the file name uses a `.darwin24` suffix instead of
    `.darwin23` in the previous version. This version also only seems to
    support ARM, so it would need to be manually updated anyway. This
    updates the version and adjusts the cask accordingly.
    
    This approach is slightly absurd (three levels of on_os blocks, wow)
    because the 3.4.2 app for ARM has a higher minimum macOS requirement
    and that wasn't accounted for in the existing setup. That is to say,
    ARM machines on Monterey or Ventura have to use the Intel app but ones
    on Sonoma can use the ARM app.

--- Casks/v/visit.rb
+++ Casks/v/visit.rb
@@ -1,33 +1,67 @@
 cask "visit" do
-  on_big_sur :or_older do
-    version "3.3.2"
-    sha256 "f406850b21ddc16a6ca636b5c9087b41baeb5460ac92a9b140194c010eb285b3"
+  on_sonoma :or_older do
+    on_big_sur :or_older do
+      version "3.3.2"
+      sha256 "f406850b21ddc16a6ca636b5c9087b41baeb5460ac92a9b140194c010eb285b3"
 
-    url "https://github.com/visit-dav/visit/releases/download/v#{version}/Visit-#{version}.dmg",
-        verified: "github.com/visit-dav/visit/"
+      url "https://github.com/visit-dav/visit/releases/download/v#{version}/Visit-#{version}.dmg",
+          verified: "github.com/visit-dav/visit/"
+
+      caveats do
+        requires_rosetta
+      end
+    end
+    on_monterey :or_newer do
+      version "3.4.2"
+
+      on_ventura :or_older do
+        sha256 "27399911756a57817e6081d3a6e22e3d569028b9261fe04bf8ee1841fddad591"
+
+        url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version.dots_to_underscores}.darwin22-x86_64.dmg",
+            verified: "github.com/visit-dav/visit/"
+
+        caveats do
+          requires_rosetta
+        end
+      end
+      on_sonoma do
+        arch arm: "23-arm64", intel: "22-x86_64"
+
+        sha256 arm:   "75b4d4bc4f8b4d9d8d0ef55e14d34e97442fbf2cf7e10b4726dd773f30b7e827",
+               intel: "27399911756a57817e6081d3a6e22e3d569028b9261fe04bf8ee1841fddad591"
+
+        url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version.dots_to_underscores}.darwin#{arch}.dmg",
+            verified: "github.com/visit-dav/visit/"
+      end
+    end
 
     livecheck do
       skip "Legacy version"
     end
-
-    caveats do
-      requires_rosetta
-    end
   end
-  on_monterey :or_newer do
-    arch arm: "23-arm64", intel: "22-x86_64"
+  on_sequoia :or_newer do
+    arch arm: "24-arm64", intel: "22-x86_64"
 
-    version "3.4.2"
-    sha256 arm:   "75b4d4bc4f8b4d9d8d0ef55e14d34e97442fbf2cf7e10b4726dd773f30b7e827",
-           intel: "27399911756a57817e6081d3a6e22e3d569028b9261fe04bf8ee1841fddad591"
+    on_arm do
+      version "3.5.0"
+      sha256 "a22479b7349a215ace6887c8b624ec2313323a744544588e4132ceb28850a1fa"
+
+      livecheck do
+        url :url
+        strategy :github_latest
+      end
+    end
+    on_intel do
+      version "3.4.2"
+      sha256 "27399911756a57817e6081d3a6e22e3d569028b9261fe04bf8ee1841fddad591"
+
+      livecheck do
+        skip "Legacy version"
+      end
+    end
 
     url "https://github.com/visit-dav/visit/releases/download/v#{version}/visit#{version.dots_to_underscores}.darwin#{arch}.dmg",
         verified: "github.com/visit-dav/visit/"
-
-    livecheck do
-      url :url
-      strategy :github_latest
-    end
   end
 
   name "VisIt"
